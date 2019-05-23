9e37e08bb96eb0b09daec73d066de1189563b187
Daniel Roth <daniel@firesideagency.ca>
2019-05-23 16:50:55 -0400

    Made port assignment dynamic

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/about', (req, res) => {
   res.send('I like biking by the beach');
 });
 
-
-app.listen( 4000, () => console.log( 'Listening on 4000') );
+const port = (process.env.PORT || 4000);
+app.listen( port, () => console.log( `Listening on ${port}`) );
